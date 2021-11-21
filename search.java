64a4baad79f08ff1e8c34c4ebc626dcfacbfb5bf
Shayan Dasgupta <shayandasgupta1502@gmail.com>
2021-11-21 19:16:58 +0530

    updated search.java
    added method for binary search

--- search.java
+++ search.java
@@ -2,34 +2,72 @@ package Algorithms;
 
 /**
  * @author Shayan Dasgupta
- * @version 1.0
+ * @version 1.1
  * @param <T>
  */
 
 public class search <T extends Comparable<T>>
 {
-    /** LINEAR SEARCH is the simplest algorithm for searching a value in an array.
-     * It takes an array <code>A</code> of size <code>n</code> and the value <code>v</code> to be searched as the parameters.
+    /** LINEAR SEARCH<br>
+     * This algorithm is the simplest algorithm for searching a value in an array.
      * A loop scans through <code>A</code> and returns the position of <code>v</code>. On the absence of <code>v</code>,
      * the function returns -1.
      * The loop invariant for this algorithm is the fact that for any ith iteration of the <code>for</code>
      * loop, the value <code>v</code> is not present in the sub-array <code>A[0...i-1]</code>.<br>
      *
-     * Time Complexity: O(n)<br>
+     * Time Complexity: \u0398(n)<br>
      * Space Complexity:
      *
-     * @param a array of type T
+     * @param A array of type T
      * @param v value to be searched
-     * @return position of the value in the array otherwise -1
+     * @return true if the value is present in <code>A</code>
      */
-    public int linear(T[] a, T v)
+    public boolean linear(T[] A, T v)
     {
-        for(int i = 0; i < a.length; i++)
-        {
-            if(a[i].equals(v))
-                return i;
+        for (T t : A) {
+            if (t.equals(v))
+                return true;
         }
 
-        return -1;
+        return false;
+    }
+
+    /**BINARY SEARCH<br>
+     * This algorithm works <strong>only</strong> for arrays that are sorted. Inorder to reduce the burden on the user
+     * to sort the array, the method sorts the array by itself by calling the method {@link sort#merge merge}.
+     * The element at the middle of the array <code>A</code> is checked against the value <code>v</code>.
+     * If <code>v</code> is greater than the midpoint then the other half of the array is discarded.
+     * We get a new midpoint element and the search begins again. Loop invariant:
+     *
+     * Time Complexity: \u0398(logn)<br>
+     * Space Complexity:
+     *
+     * @param A array of type T
+     * @param v value to be searched
+     * @return true if the value is present in <code>A</code>
+     */
+    public boolean binary(T[] A, T v)
+    {
+        //making a copy and sorting array
+        sort<T> array_sort = new sort<>();
+        T[] copy = (T[]) new Comparable[A.length];
+        System.arraycopy(A, 0, copy, 0, A.length);
+        array_sort.merge(copy, 0, copy.length-1);
+
+        int Lend = 0, Rend = A.length-1, mid;
+
+        //finding where v lies
+        while(Lend <= Rend)
+        {
+            mid = (Lend + Rend)/2;
+            if(copy[mid].compareTo(v) == 0)
+                return true;
+            else if(copy[mid].compareTo(v) < 0)
+                Lend = mid + 1;
+            else
+                Rend = mid - 1;
+        }
+
+        return false;
     }
 }

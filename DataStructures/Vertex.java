4778be88e5fe3d8aa3b0d6342c69ed8e0e400d92
Shayan Dasgupta <shayandasgupta1502@gmail.com>
2022-05-21 22:54:19 +0530

    added vertex
    general node class for any graph like data-structure

--- DataStructures/Vertex.java
+++ DataStructures/Vertex.java
@@ -1,2 +1,29 @@
-package Algorithms.DataStructures;public class Vertex {
+package Algorithms.DataStructures;
+
+public class Vertex {
+    private final String name;
+    Child children;
+
+    Vertex(String name) {
+        this.name = name;
+        children = null;
+    }
+
+    String getName() {
+        return this.name;
+    }
+}
+
+class Child {
+    private final String name;
+    Child next;
+
+    Child (String name, Child child) {
+        this.name = name;
+        next = child;
+    }
+
+    String getName() {
+        return this.name;
+    }
 }
